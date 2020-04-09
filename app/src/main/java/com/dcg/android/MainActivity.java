ff0aaa6b60216212eb056d0d9bdee7ab13067729
hoyouly <hoyouly@163.com>
2020-04-09 21:43:44 +0800

    test

--- app/src/main/java/com/dcg/android/MainActivity.java
+++ app/src/main/java/com/dcg/android/MainActivity.java
@@ -1,11 +1,17 @@
 package com.dcg.android;
 
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
 import com.dcg.android.R;
-import com.dcg.top.base.TopMvpActivity;
-import com.dcg.top.base.TopMvpPresenter;
+import com.dcg.selectphoto.SelectPhotoFragment;
+import com.dcg.top.base.*;
+import org.xutils.x;
 
 public class MainActivity extends TopMvpActivity {
+    private SelectPhotoFragment fragment;
 
     @Override
     protected int getLayoutId() {
@@ -20,5 +26,16 @@ public class MainActivity extends TopMvpActivity {
     @Override
     protected TopMvpPresenter setPresent() {
         return new TopMvpPresenter();
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        x.Ext.init(getApplication());
+//        fragment = SelectPhotoFragment.newInstance(BuildConfig.APPLICATION_ID, 10);
+//        getSupportFragmentManager()
+//            .beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .commitNowAllowingStateLoss();
     }
 }
